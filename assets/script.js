1722d00c19cb290beda0d98d851dab3006a5a74e
T Omoto <tyler.omoto@gmail.com>
2022-06-01 06:00:02 -0400

    fully functional except the load/save history

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,6 @@
 // global variables
-var apiKey = "88210bf2a33543b3ce223b8472705549"
-
-var cityLongitude;
-var cityLatitude;
-
-
-
-var searchinputEl = document.getElementById('search')
-
-var currentWeatherContainer = $("#current-weather-container");
-var currentCityName = $("#current-cityname");
-var currentIcon = $("#current-weather-icon"); 
-var currentTemperature = $("#current-temperature");
-var currentHumidity = $("#current-humidity");
-var currentWindSpeed = $("#current-windspeed");
-var currentUVIndex = $("#current-uv-index");
-
+var apiKey = "88210bf2a33543b3ce223b8472705549";
+var searchesStored = []
 
 
 
@@ -24,37 +9,46 @@ var currentWeatherBox = function(cityName){
         .then(function(response){
             // turns response into objects
             return response.json();
+            console.log(response);
         })
-        .then(function(data){
-            cityLatitude = data[0].lat 
-            cityLongitude = data [0].lon
+        .then(function(response){
+            var cityLatitude = response.coord.lat
+            var cityLongitude = response.coord.lon
 
-            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityLatitude}&lon=${cityLongitude}&appid=${apiKey}`)
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityLatitude}&lon=${cityLongitude}&exclude=minutely,hourly,alerts&units=metric&appid=${apiKey}`)
             // turn response into objects again
             .then(function(response){
                 return response.json();
+                console.log(response);
             })
             // Now use the data from response to populate current weather container
-            .then(function(respnose){
-                queryUrl(cityName);
+            .then(function(response){
+                searchHistoryList(cityName);
 
+                var currentWeatherContainer = $("#current-weather-container");
                 currentWeatherContainer.addClass("current-weather-container");
 
                 // add in data to container
                 // City and Date
+                var currentCityName = $("#current-cityname");
                 var currentDay = moment().format("M/D/YYYY");
                 currentCityName.text(`${cityName} (${currentDay})`);
                 // Icon
+                var currentIcon = $("#current-weather-icon"); 
                 currentIcon.addClass("current-weather-icon");
                 var currentIconCode = response.current.weather[0].icon;
                 currentIcon.attr("src", `https://openweathermap.org/img/wn/${currentIconCode}@2x.png`);
                 // temperature
-                currentTemperature.text("Temperature: " + response.current.temp + " \u00B0F");
+                var currentTemperature = $("#current-temperature");
+                currentTemperature.text("Temperature: " + response.current.temp + "C");
                 // humidity
+                var currentHumidity = $("#current-humidity");
                 currentHumidity.text("Humidity: " + response.current.humidity + "%");
                 // windspeed
-                currentWindSpeed.text("Wind Speed: " + response.current.wind_speed + " MPH");
+                var currentWindSpeed = $("#current-windspeed");
+                currentWindSpeed.text("Wind Speed: " + response.current.wind_speed + " m/s");
                 // uv-index with conditions for color code
+                var currentUVIndex = $("#current-uv-index");
                 currentUVIndex.text("UV Index: " + response.current.uvi);
                 
                 if (response.current.uvi < 3) {
@@ -65,7 +59,7 @@ var currentWeatherBox = function(cityName){
                     currentUVIndex.addClass("high-uv");
                 }else if (response.current.uvi >= 8 && response.current.uvi < 11) {
                     currentUVIndex.addClass("veryhigh-uv");
-                }else (response.current.uvi >= 11) {
+                }else  {
                     currentUVIndex.addClass("extreme-uv");
                 }
             })
@@ -73,6 +67,114 @@ var currentWeatherBox = function(cityName){
         // Error if user doesn't search a city name and leaves it blank
         .catch(function(err) {
             $("search-input").val("");
-            alert("Please input a city name to search for the weather.");
+            alert("Please input a valid city to search for the weather.");
         });
 };
+
+var fiveDayForecastBox = function(cityName){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
+    .then(function(response){
+        // turns response into objects
+        return response.json();
+    })
+    .then(function(response){
+        var cityLatitude = response.coord.lat
+        var cityLongitude = response.coord.lon
+
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityLatitude}&lon=${cityLongitude}&exclude=minutely,hourly,alerts&units=metric&appid=${apiKey}`)
+        // turn response into objects again
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(response){
+            // USING THIS TO TEST CAN REMOVE LATER
+            console.log(response);
+
+            var futureForecastTitleEl = $("#future-forecast-container-title");
+            futureForecastTitleEl.text("Upcoming 5-Day Forecast:")
+
+            for (var i = 1; i <= 5; i++){
+                var futureForecastCard = $(".future-forecast-card");
+                futureForecastCard.addClass("future-card-stats");
+                // date
+                var futureForecastDate = $("#future-forecast-date-" + i);
+                date = moment().add(i,"d").format("M/D/YYYY");
+                futureForecastDate.text(date);
+                // icon
+                var futureForecastIcon = $("#future-forecast-icon-" + i);
+                futureForecastIcon.addClass("future-forecast-icon");
+                var futureForecastIconCode = response.daily[i].weather[0].icon;
+                futureForecastIcon.attr("src", `https://openweathermap.org/img/wn/${futureForecastIconCode}@2x.png`);
+                // temperature
+                var futureForecastTemperature = $("#future-forecast-temperature-" + i);
+                futureForecastTemperature.text("Temp: " + response.daily[i].temp.day + "C");
+                // humidity
+                var futureForecastHumidity = $("#future-forecast-humidity-" + i);
+                futureForecastHumidity.text("Humidity: " + response.daily[i].humidity + "%");
+                // windspeed
+                var futureForecastWindSpeed = $("#future-forecast-windspeed-" + i);
+                futureForecastWindSpeed.text("Wind Speed: " + response.daily[i].wind_speed + "m/s");
+            }
+        })
+    })   
+};
+
+// Function to make a searched city part of the search history below the input
+var searchHistoryList = function(cityName) {
+    // makes new p element for city name 
+    var searchHistoryEl = $("<p>");
+    searchHistoryEl.addClass("search-history");
+    searchHistoryEl.text(cityName);
+    // gives name container
+    var searchInputContainer = $("<div>");
+    // CHECK IF NECESSARY DON'T FORGET
+    searchInputContainer.addClass("past-search-container");
+    // append to container
+    searchInputContainer.append(searchHistoryEl);
+    // append to search-history-container
+    var searchHistoryContainerEl = $("#search-history-container");
+    searchHistoryContainerEl.append(searchInputContainer);
+    
+    // Checks if array already has searches saved in local storage and gets them
+    if (searchesStored.length > 0){
+        var existingSearchesStored = localStorage.getItem("searchesStored");
+        searchesStored = JSON.parse(existingSearchesStored);
+    }
+    // Adds the city to the array of searches for storing
+    searchesStored.push(cityName);
+    localStorage.setItem("searchesStored", JSON.stringify(searchesStored));
+
+    // Then have the search input reset so the user doesn't have to manually delete
+    $("#search-input").val("");
+};
+
+// Function to load the saved local history when the user clicks the city's button
+var loadHistoryList = function(){
+    var storedSearchHistory = localStorage.getItem("searchesStored");
+    if (!storedSearchHistory){
+        return false;
+    }
+
+    storedSearchHistory = JSON.parse(storedSearchHistory);
+}
+
+$("#searchform").on("submit", function(){
+    event.preventDefault();
+    var cityName = $("#search-input").val();
+
+        currentWeatherBox(cityName);
+        fiveDayForecastBox(cityName);
+
+})
+
+$("#search-history-container").on("click", "p", function(){
+    var historyCityName = $(this).text();
+    currentWeatherBox(historyCityName);
+    fiveDayForecastBox(historyCityName);
+
+    var historyCityNamePicked = $(this);
+    historyCityNamePicked.remove();
+
+});
+
+loadSearch
